71825814ae91fcb1c8cee82e198b367e0bf7724e
aahurtado <aaronhurtado95@gmail.com>
2017-05-15 16:53:19 -0700

    Fixed login button on login page

--- public/index.js
+++ public/index.js
@@ -1,33 +1,6 @@
 $(document).ready(function () {
 
-    // console.log("Hello");
-
-    // $(".ui.dropdown").dropdown();
-
-    // $(".rating").rating();
-
-    // $(".star.rating").rating({
-    //     initialRating: 4,
-    //     maxRating: 5
-    // });
-
-    // $(".star.rating").rating("disable");
-
-    // $("#rating")
-    //     .rating({
-    //         initialRating: 4,
-    //         maxRating: 5
-    //     });
-
-    // $("#rating").rating("disable");
-
-    // $('.ui.rating')
-    //     .rating({
-    //         initialRating: 3,
-    //         maxRating: 5
-    //     });
-
-    // $('.ui.rating')
-    //     .rating();
+    $('.ui.rating')
+        .rating();
 
 });
