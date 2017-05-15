ca7372d5ff71cfc77963ae4682ba8cd334187942
aahurtado <aaronhurtado95@gmail.com>
2017-05-15 15:54:54 -0700

    Updated semantic ui from 2.2.6 to 2.2.10

--- public/index.js
+++ public/index.js
@@ -1,5 +1,33 @@
 $(document).ready(function () {
 
-    $('.ui.dropdown').dropdown();
+    // console.log("Hello");
+
+    // $(".ui.dropdown").dropdown();
+
+    // $(".rating").rating();
+
+    // $(".star.rating").rating({
+    //     initialRating: 4,
+    //     maxRating: 5
+    // });
+
+    // $(".star.rating").rating("disable");
+
+    // $("#rating")
+    //     .rating({
+    //         initialRating: 4,
+    //         maxRating: 5
+    //     });
+
+    // $("#rating").rating("disable");
+
+    // $('.ui.rating')
+    //     .rating({
+    //         initialRating: 3,
+    //         maxRating: 5
+    //     });
+
+    // $('.ui.rating')
+    //     .rating();
 
 });
