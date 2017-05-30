d7e660536b670aaa6b1535f4a4229d7a0cb6b359
Trung Do <trungdo@Trungs-iMac.local>
2017-05-29 23:29:41 -0700

    finished sign in by google

--- public/login.js
+++ public/login.js
@@ -8,12 +8,33 @@ var config = {
       };
 firebase.initializeApp(config);
 var provider = new firebase.auth.GoogleAuthProvider();
-
+console.log("done with initializing")
 
 function login(){
 	console.log("haha")
-	firebase.auth().signInWithRedirect(provider);
-
-
+	// firebase.auth().signInWithRedirect(provider);
+	//window.location="https://foody-cogs121-1492804395262.firebaseapp.com/index.html";
+firebase.auth().signInWithPopup(provider);	
 }
 
+function checkUser(){
+	firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+	  // User is signed in.
+	  console.log(user);
+	  console.log(user.email);
+
+	  name = user.displayName;
+	  console.log(name)
+
+	  document.getElementById('login-center').innerHTML = "<h2 class='ui blue image header'><p> Hi, "+name + "!</p>";
+
+	} else {
+	  // No user is signed in.
+	  console.log ("No user")
+	}
+});
+	// var user = firebase.auth().currentUser;
+
+	
+}
